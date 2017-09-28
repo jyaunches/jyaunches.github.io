e28697d218a29739a05cb2a62eedbcad1dba44dc
jyaunches <jmyaunch@gmail.com>
2017-09-27 20:06:37 -0400

    linkedlist playground to state without head represented

--- playgrounds/generics.playground/Contents.swift
+++ playgrounds/generics.playground/Contents.swift
@@ -1,16 +1,26 @@
-
-
-class Link<T>: Sequence, IteratorProtocol {
+class Link<T> {
     let value: T
-    private let nextLink: Link<T>?
+    let nextLink: Link<T>?
     
     init(_ value: T, next: Link<T>?){
         self.value = value
         self.nextLink = next
     }
+}
+
+class LinkedLink<T> : Sequence, IteratorProtocol {
+    var currentNode : Link<T>?
     
-    func next() -> Link<T>? {
-        return nextLink
+    init(head: Link<T>) {
+        currentNode = head
+    }
+    
+    func next() -> T? {
+        if let next = currentNode?.nextLink {
+            currentNode = next
+            return next.value
+        }
+        return nil
     }
 }
 
@@ -18,6 +28,12 @@ let elementFour = Link(4, next: nil)
 let elementThree = Link(3, next: elementFour)
 let elementTwo = Link(2, next: elementThree)
 let elementOne = Link(1, next: elementTwo)
+
+let linkedList = LinkedLink(head: elementOne)
+
+for item in linkedList {
+    print(item)
+}
 
 
 
